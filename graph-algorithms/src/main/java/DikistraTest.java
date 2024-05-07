31b784417f7de8f84a765a6ea439fb434b9ef379
Ahmed samir al saed <ahmedshepl4242.com>
2024-05-07 15:18:32 +0300

    merge

--- graph-algorithms/src/main/java/DikistraTest.java
+++
@@ -1,9 +0,0 @@
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-public class DikistraTest {
-
-
-}
--- graph-algorithms/src/main/java/Pair.java
+++
@@ -1,9 +0,0 @@
-public class Pair <K,V>{
-  V v;
-  K w;
-
-  public Pair( V v,K w) {
-    this.w = w;
-    this.v = v;
-  }
-}
